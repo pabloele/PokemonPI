d1ddd897380f54c630d496435d1060be87bc02e1
pabloele <pabloelevy@gmail.com>
2023-04-08 22:58:27 -0300

    removed logs

--- api/src/controllers/typesControllers.js
+++ api/src/controllers/typesControllers.js
@@ -1,50 +1,43 @@
-const {Tipo} = require('../db')
-const axios = require('axios')
+const { Tipo } = require("../db");
+const axios = require("axios");
 
 //TRAE TODOS LOS TIPOS DE LA API --> DEVUELVE UN ARRAY
 
 const getAllTypes = async () => {
-   
-   const typesArray = (await axios.get('https://pokeapi.co/api/v2/type')).data;
-   const typesNames = typesArray.results.map( e => e.name )
-   
-   return typesNames;
-}
+  const typesArray = (await axios.get("https://pokeapi.co/api/v2/type")).data;
+  const typesNames = typesArray.results.map((e) => e.name);
+
+  return typesNames;
+};
 
 //GUARDA LOS TIPOS EN LA TABLA 'Tipo' DE LA DB <-- RECIBE ARRAY DE TIPOS
 
-const typesToDb =  (typesNames) => {
-   
-   typesNames.map(async e => {
+const typesToDb = (typesNames) => {
+  typesNames.map(async (e) => {
     const type = await Tipo.create({
-            nombre: e
-        })
-
-   })
-   
-    
-}
+      nombre: e,
+    });
+  });
+};
 
 //OBTIENE EL ID DEL TIPO A PARTIR DEL NAME
 
 const idTypeByNombre = async (nombreTipo) => {
-      //console.log(nombreTipo)
-      const registro = await Tipo.findOne({
-        where: {
-          nombre: nombreTipo,
-        },
-      });
-    
-      if (!registro) {
-        return null;
-      }
-    
-      return registro.id;
-    
-}
+  const registro = await Tipo.findOne({
+    where: {
+      nombre: nombreTipo,
+    },
+  });
+
+  if (!registro) {
+    return null;
+  }
+
+  return registro.id;
+};
 
 module.exports = {
-    getAllTypes,
-    typesToDb,
-    idTypeByNombre
-}
+  getAllTypes,
+  typesToDb,
+  idTypeByNombre,
+};
--- api/src/handlers/typesHandlers.js
+++ api/src/handlers/typesHandlers.js
@@ -1,34 +1,20 @@
-
-const axios = require('axios')
-const { getAllTypes, typesToDb } = require('../controllers/typesControllers')
-
+const axios = require("axios");
+const { getAllTypes, typesToDb } = require("../controllers/typesControllers");
 
 const getTypesHandler = async (req, res) => {
-    
-    let typesNames
+  let typesNames;
 
-    try {
-        typesNames = await getAllTypes();
-        await typesToDb(typesNames);
-        //res.status(201).json({tipos_importados:typesNames})
-    } catch (error) {
-        res.status(400).json({error:error})
-    }
-    
-    //console.log(typesNames)
-    //const typesNames = getAllTypes()
-    //typesToDb()
+  try {
+    typesNames = await getAllTypes();
+    await typesToDb(typesNames);
+    //res.status(201).json({tipos_importados:typesNames})
+  } catch (error) {
+    res.status(400).json({ error: error });
+  }
 
-   
-
-    
-    res.status(200).json(typesNames)
-
-    //console.log(typesArray);
-
-
-}
+  res.status(200).json(typesNames);
+};
 
 module.exports = {
-    getTypesHandler
-}
+  getTypesHandler,
+};
